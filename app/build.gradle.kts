8f8230d9f75b180b4bd6ccaab59be07d401d3c50
Thunder <htn10a2@gmail.com>
2025-03-03 12:59:33 +0700

    add: retrofit core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
 }
