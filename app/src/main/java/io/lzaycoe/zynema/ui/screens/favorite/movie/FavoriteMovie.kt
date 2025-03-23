a21713073bb214e807918b3e8abd20b0d0a83595
Minh Vương <hardingadonis@gmail.com>
2025-03-24 01:11:12 +0700

    style: add author comments to FavoriteMovie and FavoriteTvSeries screens and view models

--- app/src/main/java/io/lzaycoe/zynema/ui/screens/favorite/movie/FavoriteMovie.kt
+++ app/src/main/java/io/lzaycoe/zynema/ui/screens/favorite/movie/FavoriteMovie.kt
@@ -1,5 +1,5 @@
 package io.lzaycoe.zynema.ui.screens.favorite.movie
-
+// Mỹ Linh
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
--- app/src/main/java/io/lzaycoe/zynema/ui/screens/favorite/tvseries/FavoriteTvSeriesViewModel.kt
+++ app/src/main/java/io/lzaycoe/zynema/ui/screens/favorite/tvseries/FavoriteTvSeriesViewModel.kt
@@ -1,5 +1,5 @@
 package io.lzaycoe.zynema.ui.screens.favorite.tvseries
-
+// Mỹ Linh
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
